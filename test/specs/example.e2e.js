d47a0003af063588b989c5dfd3794bd53e6889c9
jayashreejeg <coolrrr@gmail.com>
2021-12-16 09:04:41 -0800

    added separate page for signin and create account

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,24 @@
 const LandingPage = require('../pageobjects/landing.page');
+const SignInPage = require('../pageobjects/signin.page');
 const SecurePage = require('../pageobjects/secure.page');
 
 describe('My Login application', () => {
-    it('should login with invalid credentials', async () => {
+    it('validating the login url', async () => {
         await LandingPage.open();
         await LandingPage.gmail();
         var title= "Gmail: Free, Private & Secure Email | Google Workspace";
         await expect(browser).toHaveTitleContaining(title);
         await LandingPage.signIn();
         await expect(browser).toHaveUrlContaining("accounts");
+    });
+    it('should login with invalid gmail', async () => {
+        await LandingPage.open();
+        await LandingPage.gmail();
+        var title= "Gmail: Free, Private & Secure Email | Google Workspace";
+        await expect(browser).toHaveTitleContaining(title);
+        await LandingPage.signIn();
+        await expect(browser).toHaveUrlContaining("accounts");
+        
     });
 });
 
