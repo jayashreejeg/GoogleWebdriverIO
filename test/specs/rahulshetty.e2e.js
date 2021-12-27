151c8d20ce3d628a2f40799da4c205b574e97748
jayashreejeg <coolrrr@gmail.com>
2021-12-27 00:26:42 -0800

    chai

--- test/specs/rahulshetty.e2e.js
+++ test/specs/rahulshetty.e2e.js
@@ -1,6 +1,7 @@
 
 const { Console } = require('console')
 const fs=require('fs')
+const expectChai=require('chai').expect
 let credentials = JSON.parse(fs.readFileSync('test/testData/LoginTestData.json'))
 
 describe("rahul shetty training website", () => {
@@ -33,13 +34,79 @@ describe("rahul shetty training website", () => {
         expect(browser).toHaveTitleContaining("ProtoCommerce")
     })
 
-    it("Add to Cart test", async () => {
+    xit("login UI radio button popup test", async () => {
+        browser.url("/loginpagePractise/#")
+        console.log(browser.getTitle())
+        expect(browser).toHaveTitleContaining("LoginPage")
+        await $("#username").setValue("rahulshettyacademy")
+        await $("#password").setValue("learning")
+        //await $("#signInBtn").click()
+        //expect(browser).toHaveTitleContaining("ProtoCommerce")
+        const radioButton = await $$("//*[@name='radio']")
+        await radioButton[1].click();
+        //await browser.pause(3000)
+        await $("#cancelBtn").waitForDisplayed()
+        await $("#cancelBtn").click()
+        await browser.pause(3000)
+        console.log( await radioButton[1].isSelected()+"************")
+        await radioButton[1].click();
+        await $("#okayBtn").waitForDisplayed()
+        await $("#okayBtn").click()
+        await browser.pause(3000)
+        console.log( await radioButton[1].isSelected()+"************")
+        await radioButton[0].click();
+        await expect($(".modal-body")).not.toBeDisplayed()
+   
+    })
+
+
+    xit("login static dropdown test", async () => {
+        browser.url("/loginpagePractise/#")
+        console.log(browser.getTitle())
+        expect(browser).toHaveTitleContaining("LoginPage")
+        await $("#username").setValue("rahulshettyacademy")
+        await $("#password").setValue("learning")
+        //await $("#signInBtn").click()
+        //expect(browser).toHaveTitleContaining("ProtoCommerce")
+        const radioButton = await $$("//*[@name='radio']")
+        await radioButton[0].click();
+        await expect($(".modal-body")).not.toBeDisplayed()
+        const dropDown =await $("select.form-control")
+        dropDown.selectByAttribute("value","teach")
+        await browser.pause(3000)
+        expectChai(await dropDown.getValue()).to.equal("teach")
+    })
+
+    xit("login check box  test", async () => {
+        browser.url("/loginpagePractise/#")
+        console.log(browser.getTitle())
+        expect(browser).toHaveTitleContaining("LoginPage")
+        await $("#username").setValue("rahulshettyacademy")
+        await $("#password").setValue("learning")
+        //await $("#signInBtn").click()
+        //expect(browser).toHaveTitleContaining("ProtoCommerce")
+        const radioButton = await $$("//*[@name='radio']")
+        await radioButton[0].click();
+        await expect($(".modal-body")).not.toBeDisplayed()
+        const dropDown =await $("select.form-control")
+        dropDown.selectByAttribute("value","teach")
+        await browser.pause(3000)
+        expectChai(await dropDown.getValue()).to.equal("teach")
+    })
+
+   it("Add to Cart test", async () => {
         var products=["iphone X","Blackberry"]
         browser.url("/loginpagePractise/#")
         console.log(browser.getTitle())
         expect(browser).toHaveTitleContaining("LoginPage")
         await $("#username").setValue("rahulshettyacademy")
         await $("#password").setValue("learning")
+        const radioButton = await $$("//*[@name='radio']")
+        await radioButton[0].click();
+        await expect($(".modal-body")).not.toBeDisplayed()
+        const dropDown =await $("select.form-control")
+        await dropDown.selectByAttribute("value","teach")
+        expectChai(await dropDown.getValue()).to.equal("teach")
         await $("#signInBtn").click()
         await $("*=Checkout").waitForExist()
         const carts = await $$(".card.h-100")
@@ -66,8 +133,25 @@ describe("rahul shetty training website", () => {
 
            await $("*=Checkout").click()
            await $(".btn.btn-success").waitForExist()
-           browser.pause(3000)
-           console.log(await $("//h3/strong").getText()+"**********")
+           await browser.pause(3000)
+           console.log(`${await $("//h3/strong").getText()}**********`)
+
+                /**
+         *  const filterdList= async (dropdownList)=> {
+        await dropdownList.filter(async (item)=>{
+            const text = await item.getText()
+            return text === "India"
+       })      
+   }
+         */
+
+        await $(".btn.btn-success").click()
+        await $("//*[@value='Purchase']").waitForExist()
+        await browser.pause(3000)
+
+        await $("#country").setValue("ind")
+        await browser.pause(3000)
+
 
     })
 
